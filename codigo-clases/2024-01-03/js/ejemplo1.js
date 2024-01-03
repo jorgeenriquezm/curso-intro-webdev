c4af83fa8050ac08ae2bc34620f5b7c90ee29745
Jorge Enríquez <jorgeenriquezm@gmail.com>
2024-01-03 16:08:21 +0100

    Intro javascript iniciada

---
+++ codigo-clases/2024-01-03/js/ejemplo1.js
@@ -0,0 +1,6 @@
+
+//Comentario de una única linea
+/*En esta linea de abajo, selecciono el elemento y modifico
+su contenido*/
+document.querySelector("#elegido").innerHTML='Hola mundo!'
+console.log("Ya he modificado el div. Trabajo hecho.")
