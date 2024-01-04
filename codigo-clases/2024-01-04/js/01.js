5a8e3512a65a3c8d1459fffe6c58d0c5a676029d
Jorge Enríquez <jorgeenriquezm@gmail.com>
2024-01-04 16:15:39 +0100

    Ejemplos de bifurcación condicional y acceso a estilos con JS

---
+++ codigo-clases/2024-01-04/js/01.js
@@ -0,0 +1,31 @@
+let edad = 17
+
+//Sólo comprobamos si se cumple una condición
+if(edad>=18){
+    alert('El usuario es mayor de edad')
+}
+
+//Comprobamos si se cumple una condición y si no es así
+//tomamos una alternativa
+if (edad>=18) {
+    alert('El usuario es mayor de edad')
+} else {
+   alert('El usuario NO PUEDE ENTRAR') 
+} 
+
+
+//Aquí hacemos varias comprobaciones y si no se cumple 
+//ninguna, metemos un else
+let edad = 16
+
+if(edad==18){
+    alert("Has encontrado a Pablo")   
+}else if(edad>16){
+
+}else if(edad==36){
+
+}else if(edad>100){
+    
+}else{
+    
+}
---
+++ codigo-clases/2024-01-04/js/02.js
@@ -0,0 +1,7 @@
+//Si vamos a hacer varias tareas con un elemento, tenemos que 
+//seleccionarlo y guardarlo en un variable para consumir menos recursos
+//Una única selección es mejor que varias
+let elemento = document.querySelector("#elegido")
+elemento.style.color="red"
+elemento.style.backgroundColor="black"
+elemento.style.fontSize="32px"
