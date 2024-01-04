9b7a9ce8fd7a75b92a418299ffc14a2f5f5bb1ed
Jorge Enríquez <jorgeenriquezm@gmail.com>
2024-01-04 18:36:20 +0100

    Añadidos comentarios al javascript del ejercicio de los semáforos y al ejemplo de addEventListener

--- codigo-clases/2024-01-04/js/03.js
+++ codigo-clases/2024-01-04/js/03.js
@@ -1,4 +1,10 @@
+/*
+La función tiene que recibir dos argumentos, la edad del usuario y el cuadro que
+tiene que modificar (1-3, no controlamos otros valores)
+*/
 function checkEdad(edad, cuadro){
+    //Vamos a guardar en una variable el elemento html correspondiente al argumento
+    //que recibamos. La creamos y le damos valor según lo recibido
     let semaforo
     if(cuadro==1){
         semaforo = document.querySelector('#semaforo1')
@@ -7,7 +13,8 @@ function checkEdad(edad, cuadro){
     }else if(cuadro==3){
         semaforo = document.querySelector('#semaforo3')
     }
-    
+    //Una vez determinado sobre que cuadro tenemos que trabajar, vamos a ver
+    //que color de fondo le tenemos que dar según el primer parámetro que recibimos
     if(edad>=18){
         semaforo.style.backgroundColor='green'    
     }else{
@@ -15,6 +22,7 @@ function checkEdad(edad, cuadro){
     }
 
 }
+//Llamamos a nuestra función pasándole (edad, cuadro-a-modificar)
 checkEdad(26, 1)
 checkEdad(19, 2)
 checkEdad(36, 3)
--- codigo-clases/2024-01-04/js/04.js
+++ codigo-clases/2024-01-04/js/04.js
@@ -1,7 +1,11 @@
 let boton = document.querySelector('button')
 
-
-boton.addEventListener("click", function(){
+//Con addEventListener empezamos a escuchar un evento (primer parámetro)
+//de un elemento seleccionado previamente
+//El segundo parámetro es la función que ocurrirá cuando ocurra ese evento
+boton.addEventListener("click", function(evento){
+    //esta función recibe como parámetro "evento", que contiene toda la información
+    //del mismo. Mas adelante nos servirá para saber qué elemento hemos clicado
     console.log("Me has clicado")
     document.querySelector("body").style.backgroundColor="red"
     boton.style.display='none'
