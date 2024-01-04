410d51a38bf2c40a2a2661e4c4449beadc7be600
Jorge Enríquez <jorgeenriquezm@gmail.com>
2024-01-04 19:25:14 +0100

    Añadidos comentarios al .js del ejercicio de los botones

--- codigo-clases/2024-01-04/js/05.js
+++ codigo-clases/2024-01-04/js/05.js
@@ -2,12 +2,19 @@ const semaforo1 = document.querySelector('#semaforo1')
 const semaforo2 = document.querySelector('#semaforo2')
 const semaforo3 = document.querySelector('#semaforo3')
 
-document.querySelector('#boton1').addEventListener('click', function(){
+document.querySelector('#boton1').addEventListener('click', function(ev){
+    //Esta función que se ejecuta tras el click recibe un parámetro "ev"
+    //Ese parametro no da muchísima información
+    console.log(ev) //nos saca TODA la info del evento
+    console.log(ev.target) //nos dice que elemento se ha clicado
     semaforo1.innerHTML=''
+    semaforo1.style.backgroundColor="#f5909f"
 })
 document.querySelector('#boton2').addEventListener('click', function(){
     semaforo2.innerHTML=''
+    semaforo2.style.backgroundColor="#f5909f"
 })
 document.querySelector('#boton3').addEventListener('click', function(){
     semaforo3.innerHTML=''
+    semaforo3.style.backgroundColor="#f5909f"
 })
