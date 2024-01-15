bb7ea4e9424a071b290af2da01c83d25010a70b1
Jorge Enríquez <jorgeenriquezm@gmail.com>
2024-01-15 18:45:02 +0100

    Ejemplo de añadido dinámico de options a un select

---
+++ codigo-clases/2024-01-15/js/02.js
@@ -0,0 +1,11 @@
+let selector = document.querySelector('select')
+
+let op3 = document.createElement("option");
+op3.text = "Opcion 3";
+op3.value=3
+selector.add(op3);
+
+let op4 = document.createElement("option");
+op4.text = "Opcion 4";
+op4.value=4
+selector.add(op4);
