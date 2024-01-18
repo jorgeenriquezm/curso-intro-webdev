0613d331f0913b84a17c51a976910ac07672f827
jorgeenriquezm <jorgeenriquezm@gmail.com>
2024-01-18 10:55:56 +0100

    Enunciado ejercicio Simon dice

---
+++ codigo-clases/2024-01-18/js/simon.js
@@ -0,0 +1,7 @@
+//1 - Cada click en uno de los cuatro pulsadores, deberá AÑADIR a un array guardado en localStorage el color que se ha pulsado.
+//Debería resultar algo como ['rojo', 'rojo', 'azul', 'amarillo', 'rojo']...
+
+//2 - El boton de Reset tiene que borrar las pulsaciones guardadas (no el elemento de localStorage)
+
+//3 - El botón de Replay tiene que reproducir la secuencia guardada, haciendo que se iluminen los botones correspondientes.
+//(Podéis utilizar un setInterval para tal efecto)
