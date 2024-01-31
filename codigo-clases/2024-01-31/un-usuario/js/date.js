ed2ee9b4f9d4f30c6185aad35f0a23d1e7b8ddff
Jorge Enríquez <jorgeenriquezm@gmail.com>
2024-01-31 19:26:00 +0100

    Codigo del día

---
+++ codigo-clases/2024-01-31/un-usuario/js/date.js
@@ -0,0 +1,6 @@
+let cadena = "1974-04-08T17:58:09.137Z"
+/* let dividida = cadena.split('T')
+console.log(dividida[0]) */
+//console.log(cadena.substring(0,10))
+const fecha = new Date("1974-04-08T17:58:09.137Z")
+console.log(fecha.getFullYear())
