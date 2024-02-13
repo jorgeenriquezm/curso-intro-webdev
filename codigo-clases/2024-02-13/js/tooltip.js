8c1fb3938b244980055ed4fa03009621f6f890f4
Jorge Enríquez <jorgeenriquezm@gmail.com>
2024-02-13 19:37:54 +0100

    Corrección en las coordenadas del mouse

--- codigo-clases/2024-02-13/js/tooltip.js
+++ codigo-clases/2024-02-13/js/tooltip.js
@@ -2,12 +2,12 @@ let posX
 let posY
 const tooltip = document.querySelector('.tooltip')
 document.querySelector('body').addEventListener('mousemove',(e)=>{
-    posX = e.screenX
-    posY = e.screenY
+    posX = e.clientX
+    posY = e.clientY
 })
 document.querySelector('input').addEventListener('input', (e)=>{
     tooltip.style.left=`${posX}px`
-    tooltip.style.top=`${posY-100}px`
+    tooltip.style.top=`${posY-30}px`
     tooltip.style.display='block'
     tooltip.innerHTML=e.target.value
 })
