39a73c2750dc94aeaf726ee9b5d8e5774f8768f2
Jorge Enríquez <jorgeenriquezm@gmail.com>
2024-03-05 16:55:36 +0100

    API modificada con soporte para CORS

--- node/mi-portfolio/api/index.js
+++ node/mi-portfolio/api/index.js
@@ -1,11 +1,17 @@
 //Instalamos lo módulos necesarios, express y mysql
 const express = require('express')
-var mysql = require('mysql');
+var mysql = require('mysql')
+//Instalamos CORS que nos hace falta para poder trabajar en loca
+//Ahora vamos a autorizar todas las peticiones que nos lleguen
+//Luego en producción tenemos que quitar esto
+var cors = require('cors')
 //y el middleware para poder trabajar con json
 var json = require('json');
 //Inicializamos app
 const app = express()
 const port = 3000
+//Le decimos que usaremos estos cors
+app.use(cors())
 //Inicializamos la conexión con la BBDD
 var connection = mysql.createConnection({
   host     : 'localhost',
